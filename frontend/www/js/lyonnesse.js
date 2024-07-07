114d2d2025366cedceddab66be270b7b1e23804a
Phoenix <86477740+yesitsPhoenix@users.noreply.github.com>
2024-07-07 18:02:41 -0500

    map view fix

--- frontend/www/js/lyonnesse.js
+++ frontend/www/js/lyonnesse.js
@@ -39,7 +39,7 @@ async function fetchData(fileName) {
             zoomSnap: 0.1,
             crs: L.CRS.Simple
         }).setView(
-            [0, 0], 2);
+            [0, 0], 1);
         map.on("zoomend", function() {
             if (map.getZoom() > map.options.maxZoom) {
                 map.setZoom(map.options.maxZoom);
@@ -93,7 +93,7 @@ async function fetchData(fileName) {
             },
   
             _zoomHome: function(e) {
-                this._map.setView([0, 0], 2, {
+                this._map.setView([0, 0], 1, {
                     animate: true
                 });
             },
