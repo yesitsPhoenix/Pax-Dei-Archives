0bf57cf16001d2f37f0ae0e3a1c6bafbbfda9185
yesitsPhoenix <86477740+yesitsPhoenix@users.noreply.github.com>
2025-09-19 11:20:12 -0500

    Update map.js

--- frontend/www/js/map.js
+++ frontend/www/js/map.js
@@ -126,8 +126,8 @@ async function initMap() {
 
         var imageOverlay = L.imageOverlay(
             "frontend/www/assets/combined_dark1.jpg", [
-                [700, -520],
-                [-700, 520],
+                [680, -520],
+                [-680, 520],
             ]
         ).addTo(map);
         imageOverlay.getElement().classList.add('image-overlay');
