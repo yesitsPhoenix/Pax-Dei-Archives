9e52881df2b1f6c4c99a1177dc0faa4b71cc2e91
Phoenix <86477740+yesitsPhoenix@users.noreply.github.com>
2025-12-05 12:51:56 -0600

    Update supabaseClient.js

--- frontend/www/js/supabaseClient.js
+++ frontend/www/js/supabaseClient.js
@@ -1,5 +1,5 @@
 // supabaseClient.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.85.0/+esm';
 
 const SUPABASE_URL = 'https://jrjgbnopmfovxwvtbivh.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impyamdibm9wbWZvdnh3dnRiaXZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDgxOTg1MjYsImV4cCI6MjAyMzc3NDUyNn0.za7oUzFhNmBdtcCRBmxwW5FSTFRWVAY6_rsRwlr3iqY';
