f8967c1f0f6a203dbbebe3d80463458f53c9a63f
Phoenix <86477740+yesitsPhoenix@users.noreply.github.com>
2026-02-22 10:51:11 -0600

    quest box fix

--- frontend/www/js/redemption/editQuest.js
+++ frontend/www/js/redemption/editQuest.js
@@ -1,7 +1,6 @@
 import { supabase } from "../supabaseClient.js";
 import { enableSignTooltip, mouseTooltip } from '../ui/signTooltip.js';
 import { questState } from './questStateManager.js';
-import { initializeMarkdownToolbar } from '../ui/markdownToolbar.js';
 import { 
     createAdditionalCategoriesUI, 
     getSelectedAdditionalCategories,
@@ -386,7 +385,6 @@ async function loadEditor(userId, signData) {
     await loadPrerequisiteOptionsForEdit('hard-lock-container', quest.hard_lock_quest_ids || [], 'hard-lock-quest', 'hard-lock-search', 'hard-lock-list');
     await loadPrerequisiteOptionsForEdit('prerequisite-container', quest.prerequisite_quest_ids || [], 'prereq-quest', 'prereq-search', 'prereq-list');
 
-    initializeMarkdownToolbar();
     setupEditorEvents(baseUrl, version);
 }
 
